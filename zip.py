2e9495e2185f4b073138ff0a01673d1bc50a7446
zuxel <a.borisov7386@gmail.com>
2021-02-27 22:08:11 +0700

    Создание папки, если ее нет

--- zip.py
+++ zip.py
@@ -10,21 +10,55 @@
 # если у неё есть интерфейс командной строки, чтобы ей можно было
 # передавать аргументы из нашего сценария
 
+# Версия 1
+# import os
+# import time
+#
+# source_dir = ["/home/zuxel/copy"]
+# files = os.listdir(source_dir[0])
+# print(files)
+#
+# target_dir = "/home/zuxel/back"
+#
+# target_name = target_dir + os.sep + time.strftime('%Y-%m-%d-%H%M%S') + '.zip'
+#
+# zip_command = "zip -r {0} {1}".format(target_name, " ".join(source_dir))
+#
+# if os.system(zip_command) == 0:
+#     print("Done, лежит в", target_name)
+# else:
+#     print("Что то пошло не так, чувак")
+
+# Версия 2
 import os
 import time
 
+# Откуда копируем
 source_dir = ["/home/zuxel/copy"]
-files = os.listdir(source_dir[0])
-print(files)
-
+# Куда копируем
 target_dir = "/home/zuxel/back"
-
-target_name = target_dir + os.sep + time.strftime('%Y-%m-%d-%H%M%S') + '.zip'
-
+# Если папки назначения не существует, создаём
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+    print("Папка назначения создана")
+else:
+    print("Папка назначения сущестует")
+# Имя папки = дата архивации
+folder_name = target_dir + os.sep + time.strftime("%Y%m%d")
+# Имя файла с архивом
+file_name = time.strftime("%H%M%S")
+# Создаём папку внутри папки назначения, если ее ещё нет
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+    print("Папка создана")
+else:
+    print("Папка для текущей даты уже есть, новую не создаём")
+# Собираем имя файла назначения из имени папки, разделителя, имени файла и расширения .zip
+target_name = folder_name + os.sep + file_name + '.zip'
+# Собираем zip команду
 zip_command = "zip -r {0} {1}".format(target_name, " ".join(source_dir))
-
+# Выполняем арживацию
 if os.system(zip_command) == 0:
     print("Done, лежит в", target_name)
 else:
-    print("Что то пошло не так, чувак")
-
+     print("Что то пошло не так, чувак")
